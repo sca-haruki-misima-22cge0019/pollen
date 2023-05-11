34ab257b16265ae99a8edb8f04622e7e8925708c
Haruki-Misima <test>
2023-05-11 14:41:11 +0900

    画面タッチでシーン移動を変更して、ボタンの配置と移動、操作説明画面の仮置きも作ってみました。

---
+++ Assets/Misima/Script/Operation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Operation : MonoBehaviour
+{
+    public void onClickOperationButton()
+    {
+        SceneManager.LoadScene("Operation explanation");
+    }
+}
