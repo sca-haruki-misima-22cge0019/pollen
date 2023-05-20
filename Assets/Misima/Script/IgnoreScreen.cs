dd8e5f7d439b8531b1eb381e81b385c0a0e5228c
PG22-01 <20cge0011st@edu.sendai-com.ac.jp>
2023-05-20 16:00:13 +0900

    画面すべて仮置きの状態で完成

--- Assets/Misima/Script/IgnoreScreen.cs
+++ Assets/Misima/Script/IgnoreScreen.cs
@@ -5,23 +5,27 @@ using UnityEngine.EventSystems;
 
 public class IgnoreScreen : MonoBehaviour
 {
-    private void Start()
-    {
+    void Start() {
+        //EventTriggerコンポーネントを取得
         EventTrigger eventTrigger = gameObject.GetComponent<EventTrigger>();
 
+        //イベントの設定に入る
         EventTrigger.Entry entry = new EventTrigger.Entry();
 
+        //PointerDown(押した瞬間に実行する)イベントタイプを設定
         entry.eventID = EventTriggerType.PointerDown;
 
-        entry.callback.AddListener((x) =>
+        //関数を設定
+        entry.callback.AddListener((x) => 
         {
             Trigger();
         });
 
+        //イベントの設定をEventTriggerに反映
         eventTrigger.triggers.Add(entry);
     }
 
-    public void Trigger()
+    public void Trigger() 
     {
         Debug.Log("Trigger");
     }
