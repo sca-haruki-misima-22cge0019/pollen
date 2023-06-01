c29d6059e949938e6133c76f0298d388ed9d074f
haruki-misima <sdt22cge0019@edu.sca.ac.jp>
2023-06-01 14:46:01 +0900

    タイトルシーンに画像入れてみました

---
+++ Assets/Misima/Script/SpecailBullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecailBullet : MonoBehaviour
+{
+    [SerializeField] GameObject sphereObject;
+
+    // Start is called before the first frame update
+    void Start()
+     {
+        Invoke("SelfDestroy",30.0f);
+
+        sphereObject.SetActive(false);
+        Invoke("SphereSet", 1.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(-0.1f,0,0));
+    }
+
+    void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+    void SphereSet()
+    {
+        sphereObject.SetActive(true);
+    }
+}
