990121e9ec3763f6debdba03941875f88db68f8b
haruki-misima <sdt22cge0019@edu.sca.ac.jp>
2023-07-15 14:49:17 +0900

    タイトル動かすのいったん停止

--- Assets/Misima/Script/BackG.cs
+++ Assets/Misima/Script/BackG.cs
@@ -4,33 +4,32 @@ using UnityEngine;
 
 public class BackG : MonoBehaviour
 {
-    //バックグラウンドを動かす
-
     [SerializeField]
     float scrollSpeed = -1;
 
-    Vector2 cameraRecrMin;
+    Vector3 cameraRectMin;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 1;
-        //カメラ範囲の取得
-        cameraRecrMin = Camera.main.ViewportToWorldPoint(new Vector2(Camera.main.transform.position.x,0));
+        //カメラの範囲を取得
+        cameraRectMin = Camera.main.ViewportToWorldPoint
+            (new Vector3(0, 0, Camera.main.transform.position.z));
     }
-
-    // Update is called once per frame
     void Update()
     {
         Move();
     }
-    private void Move()
+    void Move()
     {
-        transform.Translate(Vector2.right * scrollSpeed * Time.deltaTime);//x軸方向にスクロール
-        //カメラの左端が完全に出たら、右端に瞬間移動
-        if (transform.position.x < (cameraRecrMin.x - Camera.main.transform.position.x) * 2)
+        transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
+        //X軸方向にスクロール
+
+        //カメラの左端から完全に出たら、右端に瞬間移動
+        if (transform.position.x < (cameraRectMin.x - Camera.main.transform.position.x) * 2)
+
         {
-            transform.position = new Vector2((Camera.main.transform.position.x - cameraRecrMin.x)*2,
+            transform.position = new Vector2
+                ((Camera.main.transform.position.x - cameraRectMin.x) * 2,
                 transform.position.y);
         }
     }
