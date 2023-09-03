094e4123d64b58ce4eea3addc1e18a1b57c36cb0
sca-yua-nakamura0014 <21cge0014ny@edu.sca.ac.jp>
2023-09-03 20:45:03 +0900

    ボタンを押した時の効果音完了

--- Assets/Misima/Script/SceneChenge.cs
+++ Assets/Misima/Script/SceneChenge.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneChenge : MonoBehaviour
 {    // Update is called once per frame
+    bool sound = false;
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        AudioSource audio = GetComponent<AudioSource>();
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            sound = true;
+            audio.Play();
+            
+        }
+
+        if (!audio.isPlaying && sound)
         {
             SceneManager.LoadScene("TestScene");
         }
