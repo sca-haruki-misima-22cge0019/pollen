f0cd884908504ba0b7404c34c8a03d37013acafc
sca-yua-nakamura0014 <21cge0014ny@edu.sca.ac.jp>
2023-09-05 19:46:04 +0900

    効果音修正

--- Assets/Misima/Script/SceneChenge.cs
+++ Assets/Misima/Script/SceneChenge.cs
@@ -5,18 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneChenge : MonoBehaviour
 {    // Update is called once per frame
-    bool sound = false;
     void Update()
     {
-        AudioSource audio = GetComponent<AudioSource>();
         if (Input.GetKeyDown(KeyCode.Return))
-        {
-            sound = true;
-            audio.Play();
-            
-        }
-
-        if (!audio.isPlaying && sound)
         {
             SceneManager.LoadScene("TestScene");
         }
