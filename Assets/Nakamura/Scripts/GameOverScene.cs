5e26069ccdcccf23190f725d4550e608b75c7379
PG22-01 <20cge0011st@edu.sendai-com.ac.jp>
2023-09-30 14:04:27 +0900

    ゲームオーバー時の花粉の修正

--- Assets/Nakamura/Scripts/GameOverScene.cs
+++ Assets/Nakamura/Scripts/GameOverScene.cs
@@ -5,20 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScene : MonoBehaviour
 {
+    public bool des;
     // Start is called before the first frame update
     void Start()
     {
-        
+        des = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void End()
     {
         SceneManager.LoadScene("GameOverScene");
+    }
+
+    void Destroy()
+    {
+        des = true;
     }
 }
